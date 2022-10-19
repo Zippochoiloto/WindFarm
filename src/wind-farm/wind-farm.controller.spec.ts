7e5c381ab79d5b4e38a40c48966ba14ae9d2a739
Zippochoiloto <theanhptit94@gmail.com>
2022-10-19 20:45:50 +0700

    add more unit test

--- src/wind-farm/wind-farm.controller.spec.ts
+++ src/wind-farm/wind-farm.controller.spec.ts
@@ -25,8 +25,45 @@ describe('WindFarmController', () => {
     controller = module.get<WindFarmController>(WindFarmController);
   });
 
-  it('should be defined', async () => {
+  it('get all windfarm', async () => {
     const respond = await controller.getAllWindFarm();
     if (respond.length > 0) return true;
+  });
+
+  it('get windfarm by id', async () => {
+    const result = {
+      _id: '634ff736507a9116c325bd88',
+      siteName: 'First site',
+      windSpeed: 50,
+      windDirection: 'South',
+      status: 'normal',
+      activePower: 10,
+      maxPower: 100,
+      percentageActive: 0.1,
+      __v: 0,
+    };
+    const respond = await controller.getWindFarm({
+      id: '634ff736507a9116c325bd88',
+    });
+    console.log('respond', respond);
+  });
+
+  it('create windfarm', async () => {
+    const respond = await controller.createWindFarm({
+      siteName: 'Fifth site',
+      windSpeed: 50,
+      windDirection: 'South',
+      status: 'normal',
+      activePower: 10,
+      maxPower: 100,
+    });
+    console.log('respond', respond);
+  });
+
+  it('delete windfarm', async () => {
+    const respond = await controller.deleteWindFarm({
+      id: '634ff736507a9116c325bd88',
+    });
+    console.log('respond', respond);
   });
 });
